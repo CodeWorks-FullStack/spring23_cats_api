44016d14a55ce8795652217beb4e2c9f117cdeaa
Jeremy Fowler <jembyfowler@gmail.com>
2023-03-20 14:47:59 -0600

    student handoff

--- server/controllers/CatsController.js
+++ server/controllers/CatsController.js
@@ -6,7 +6,10 @@ export class CatsController extends BaseController {
     super('api/cats')
     this.router
       .get('', this.getCats)
+      // NOTE a colon in our mount path denotes a variable that can be targeted in the request parameters (req.params)
       .get('/:catId', this.getCatById)
+      .post('', this.createCat)
+      .delete('/:catId', this.deleteCat)
   }
 
   // NOTE req is the request a user sends to the api
@@ -17,7 +20,8 @@ export class CatsController extends BaseController {
 
   async getCats(req, res, next) {
     try {
-      const cats = await catsService.getCats()
+      const query = req.query
+      const cats = await catsService.getCats(query)
       return res.send(cats)
     } catch (error) {
       next(error)
@@ -33,4 +37,29 @@ export class CatsController extends BaseController {
       next(error)
     }
   }
+
+  async createCat(req, res, next) {
+    try {
+      // NOTE                 VVVVVVVVVV this is our request body
+      // api.post('api/cars', {make: 'mazda'})
+      // NOTE 99% of the time, post reequests have a body
+      const catData = req.body
+      const cat = await catsService.createCat(catData)
+      return res.send(cat)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteCat(req, res, next) {
+    try {
+      // NOTE delete requests do not have a request body
+      const catId = req.params.catId
+      await catsService.deleteCat(catId)
+      return res.send('Deleted cat')
+    } catch (error) {
+      next(error)
+    }
+  }
+
 }
--- server/services/CatsService.js
+++ server/services/CatsService.js
@@ -1,7 +1,26 @@
-import { fakeDB } from "../db/FakeDB.js"
+import { Cat, fakeDB } from "../db/FakeDB.js"
 import { BadRequest } from "../utils/Errors.js"
 
 class CatsService {
+  async deleteCat(catId) {
+    const catIndex = await fakeDB.cats.findIndex(c => c.id == catId)
+
+    if (catIndex == -1) {
+      throw new BadRequest("NO CAT FOUND")
+    }
+
+    fakeDB.cats.splice(catIndex, 1)
+  }
+  async createCat(catData) {
+
+    if (!catData.name || !catData.id || !catData.color) {
+      throw new BadRequest("No cat data found")
+    }
+
+    const builtCat = new Cat(catData)
+    await fakeDB.cats.push(builtCat)
+    return builtCat
+  }
   async getCatById(catId) {
     const cat = await fakeDB.cats.find(c => c.id == catId)
 
@@ -11,8 +30,16 @@ class CatsService {
 
     return cat
   }
-  async getCats() {
-    const cats = await fakeDB.cats
+  async getCats(query) {
+    let cats = []
+
+    if (query.color) {
+      cats = await fakeDB.cats.filter(c => c.color.toLowerCase() == query.color.toLowerCase())
+    }
+    else {
+      cats = await fakeDB.cats
+    }
+
     return cats
   }
 
